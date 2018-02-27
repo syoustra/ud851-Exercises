2b2f9c9f611c2be6528a9674c7f67ed0014724c4
Stephanie Youstra <steph.you07@gmail.com>
2018-02-27 16:37:58 -0500

    feat: Add layout resource (Lesson 4.10)

--- Lesson03-Green-Recycler-View/T03.02-Exercise-ViewHolder/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
+++ Lesson03-Green-Recycler-View/T03.02-Exercise-ViewHolder/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
@@ -37,19 +37,19 @@ import android.widget.TextView;
  */
 public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHolder> {
 
-    // TODO (1) Create a layout resource in res/layout/ called number_list_item.xml
+    // COMPLETED (1) Create a layout resource in res/layout/ called number_list_item.xml
 
     // Do steps 2 - 11 within number_list_item.xml
-    // TODO (2) Make the root layout a FrameLayout
-    // TODO (3) Make the width match_parent and the height wrap_content
-    // TODO (4) Set the padding to 16dp
-    // TODO (5) Add a TextView as the only child of the FrameLayout
-    // TODO (6) Give the TextView an ID "@+id/tv_item_number"
-    // TODO (7) Set the height and width to wrap_content
-    // TODO (8) Align the TextView to the start of the parent
-    // TODO (9) Center the TextView vertically in the layout
-    // TODO (10) Set the font family to monospace
-    // TODO (11) Set the text size to 42sp
+    // COMPLETED (2) Make the root layout a FrameLayout
+    // COMPLETED (3) Make the width match_parent and the height wrap_content
+    // COMPLETED (4) Set the padding to 16dp
+    // COMPLETED (5) Add a TextView as the only child of the FrameLayout
+    // COMPLETED (6) Give the TextView an ID "@+id/tv_item_number"
+    // COMPLETED (7) Set the height and width to wrap_content
+    // COMPLETED (8) Align the TextView to the start of the parent
+    // COMPLETED (9) Center the TextView vertically in the layout
+    // COMPLETED (10) Set the font family to monospace
+    // COMPLETED (11) Set the text size to 42sp
 
     private static final String TAG = GreenAdapter.class.getSimpleName();
 
