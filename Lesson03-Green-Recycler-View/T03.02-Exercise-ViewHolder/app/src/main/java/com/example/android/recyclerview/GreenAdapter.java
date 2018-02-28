750b64fe44a4165a27dc1e122bce3c044ac4913e
Stephanie Youstra <steph.you07@gmail.com>
2018-02-28 11:26:02 -0500

    feat: Add bind helper method (Lesson 4.10)

--- Lesson03-Green-Recycler-View/T03.02-Exercise-ViewHolder/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
+++ Lesson03-Green-Recycler-View/T03.02-Exercise-ViewHolder/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
@@ -130,10 +130,13 @@ public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHo
             listItemNumberView = (TextView) itemView.findViewById(R.id.tv_item_number);
         }
 
+        // COMPLETED (16) Within the NumberViewHolder class, create a void method called bind that accepts an int parameter called listIndex
+        // COMPLETED (17) Within bind, set the text of listItemNumberView to the listIndex
+        // COMPLETED (18) Be careful to get the String representation of listIndex, as using setText with an int does something different
 
-        // TODO (16) Within the NumberViewHolder class, create a void method called bind that accepts an int parameter called listIndex
-        // TODO (17) Within bind, set the text of listItemNumberView to the listIndex
-        // TODO (18) Be careful to get the String representation of listIndex, as using setText with an int does something different
+        void bind(int listIndex) {
+            listItemNumberView.setText(String.valueOf(listIndex));
+        }
 
     }
 }
