d0069e090cf08e085669c6c0881755b1cf9bd3d7
Stephanie Youstra <steph.you07@gmail.com>
2018-03-08 12:34:50 -0500

    feat: Retrieve and display data from Intent (Lesson 5.09)

--- Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/ChildActivity.java
+++ Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/ChildActivity.java
@@ -15,9 +15,12 @@
  */
 package com.example.android.explicitintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ChildActivity extends AppCompatActivity {
 
@@ -32,12 +35,17 @@ public class ChildActivity extends AppCompatActivity {
         /* Typical usage of findViewById... */
         mDisplayText = (TextView) findViewById(R.id.tv_display);
 
-        // TODO (3) Use the getIntent method to store the Intent that started this Activity in a variable
+        // COMPLETED (3) Use the getIntent method to store the Intent that started this Activity in a variable
+        Intent originatingIntent = getIntent();
 
-        // TODO (4) Create an if statement to check if this Intent has the extra we passed from MainActivity
+        // COMPLETED (4) Create an if statement to check if this Intent has the extra we passed from MainActivity
+        if (originatingIntent.hasExtra(Intent.EXTRA_TEXT)) {
 
-            // TODO (5) If the Intent contains the correct extra, retrieve the text
+            // COMPLETED (5) If the Intent contains the correct extra, retrieve the text
+            String textEntered = originatingIntent.getStringExtra(Intent.EXTRA_TEXT);
 
-            // TODO (6) If the Intent contains the correct extra, use it to set the TextView text
+            // COMPLETED (6) If the Intent contains the correct extra, use it to set the TextView text
+            mDisplayText.setText(textEntered);
+        }
     }
 }
--- Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/MainActivity.java
+++ Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
              */
             @Override
             public void onClick(View v) {
-                // TODO (1) Retrieve the text from the EditText and store it in a variable
+                // COMPLETED (1) Retrieve the text from the EditText and store it in a variable
                 String textEntered = mNameEntry.getText().toString();
 
                 /*
@@ -74,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
                  */
                 Intent startChildActivityIntent = new Intent(context, destinationActivity);
 
-                // TODO (2) Use the putExtra method to put the String from the EditText in the Intent
+                // COMPLETED (2) Use the putExtra method to put the String from the EditText in the Intent
                 startChildActivityIntent.putExtra(Intent.EXTRA_TEXT, textEntered);
 
                 /*
