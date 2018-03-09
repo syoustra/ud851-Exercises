4ec6fd3f1f14c3e66f55e621e7df42d5ae80ee39
Stephanie Youstra <steph.you07@gmail.com>
2018-03-09 00:54:18 -0500

    feat: Create implicit intent (Lesson 5.12)

--- Lesson04b-Webpages-Maps-and-Sharing/T04b.01-Exercise-OpenWebpage/app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ Lesson04b-Webpages-Maps-and-Sharing/T04b.01-Exercise-OpenWebpage/app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -15,6 +15,8 @@
  */
 package com.example.android.implicitintents;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -79,10 +81,18 @@ public class MainActivity extends AppCompatActivity {
 
     // TODO (1) Create a method called openWebPage that accepts a String as a parameter
     // Do steps 2 - 4 within openWebPage
+    private void openWebPage(String webPage) {
 
         // TODO (2) Use Uri.parse to parse the String into a Uri
+        Uri uri = Uri.parse(webPage);
 
         // TODO (3) Create an Intent with Intent.ACTION_VIEW and the webpage Uri as parameters
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
 
         // TODO (4) Verify that this Intent can be launched and then call startActivity
+        if(intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+    }
 }
