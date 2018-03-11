845fa9ed2201e67b906c7f2564ece5a21145b0c3
Stephanie Youstra <steph.you07@gmail.com>
2018-03-11 12:33:40 -0400

    feat: Save Search status during onSaveInstance (Lesson 6.12)

--- Lesson05b-Smarter-GitHub-Repo-Search/T05b.01-Exercise-SaveResults/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
+++ Lesson05b-Smarter-GitHub-Repo-Search/T05b.01-Exercise-SaveResults/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
@@ -17,6 +17,7 @@ package com.example.android.asynctaskloader;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -29,12 +30,15 @@ import com.example.android.asynctaskloader.utilities.NetworkUtils;
 
 import java.io.IOException;
 import java.net.URL;
+import java.security.Key;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO (1) Create a static final key to store the query's URL
+    private static final String SEARCH_RESULTS_QUERY_URL = "query";
 
     // TODO (2) Create a static final key to store the search's raw JSON
+    private static final String SEARCH_RESULTS_RAW_JSON = "results";
 
     private EditText mSearchBoxEditText;
 
@@ -60,6 +64,10 @@ public class MainActivity extends AppCompatActivity {
         mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
 
         // TODO (9) If the savedInstanceState bundle is not null, set the text of the URL and search results TextView respectively
+        if (savedInstanceState != null) {
+            mUrlDisplayTextView.setText(savedInstanceState.getString(SEARCH_RESULTS_QUERY_URL));
+            mSearchResultsTextView.setText(savedInstanceState.getString(SEARCH_RESULTS_RAW_JSON));
+        }
     }
 
     /**
@@ -153,11 +161,20 @@ public class MainActivity extends AppCompatActivity {
 
     // TODO (3) Override onSaveInstanceState to persist data across Activity recreation
     // Do the following steps within onSaveInstanceState
-    // TODO (4) Make sure super.onSaveInstanceState is called before doing anything else
 
-    // TODO (5) Put the contents of the TextView that contains our URL into a variable
-    // TODO (6) Using the key for the query URL, put the string in the outState Bundle
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        // TODO (4) Make sure super.onSaveInstanceState is called before doing anything else
+        super.onSaveInstanceState(outState, outPersistentState);
 
-    // TODO (7) Put the contents of the TextView that contains our raw JSON search results into a variable
-    // TODO (8) Using the key for the raw JSON search results, put the search results into the outState Bundle
+        // TODO (5) Put the contents of the TextView that contains our URL into a variable
+        // TODO (6) Using the key for the query URL, put the string in the outState Bundle
+        String urlToSave = mUrlDisplayTextView.getText().toString();                            //GOT .getText FROM VIDEO
+        outState.putString(SEARCH_RESULTS_QUERY_URL, urlToSave);
+
+        // TODO (7) Put the contents of the TextView that contains our raw JSON search results into a variable
+        // TODO (8) Using the key for the raw JSON search results, put the search results into the outState Bundle
+        String jsonToSave = mSearchResultsTextView.toString();                                  //GOT .getText FROM VIDEO
+        outState.putString(SEARCH_RESULTS_RAW_JSON, jsonToSave);
+    }
 }
