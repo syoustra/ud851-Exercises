fea1a6345a4d4f8fe586e2611d2b4868094bc483
Stephanie Youstra <steph.you07@gmail.com>
2018-03-16 11:56:50 -0400

    feat: Add CheckboxPreference to show bass (Lesson 7.10)

---
+++ Lesson06-Visualizer-Preferences/T06.02-Exercise-MakeAPreferenceFragment/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -0,0 +1,15 @@
+package android.example.com.visualizerpreferences;
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+/**
+ * Created by stephanieyoustra on 3/16/18.
+ */
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+
+    }
+}
