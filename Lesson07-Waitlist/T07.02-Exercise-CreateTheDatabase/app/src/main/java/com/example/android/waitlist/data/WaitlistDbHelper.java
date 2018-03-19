cecdeb0fe8c03b8f5bacc4c03f427b885c7159c3
Stephanie Youstra <steph.you07@gmail.com>
2018-03-19 01:41:18 -0400

    feat: Set database name and version and constructor (Lesson 8.08)

--- Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
+++ Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
@@ -1,13 +1,24 @@
 package com.example.android.waitlist.data;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 // TODO (1) extend the SQLiteOpenHelper class
-public class WaitlistDbHelper {
+public class WaitlistDbHelper extends SQLiteOpenHelper {
 
     // TODO (2) Create a static final String called DATABASE_NAME and set it to "waitlist.db"
+    private static final String DATABASE_NAME = "waitlist.db";
 
     // TODO (3) Create a static final int called DATABASE_VERSION and set it to 1
+    private static final int DATABASE_VERSION = 1;
 
     // TODO (4) Create a Constructor that takes a context and calls the parent constructor
+
+    public WaitlistDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
 
     // TODO (5) Override the onCreate method
 
