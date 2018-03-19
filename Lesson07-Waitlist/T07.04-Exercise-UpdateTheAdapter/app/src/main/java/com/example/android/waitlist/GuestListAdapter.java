869a53d04211d5b7dab958c6e191ea31cf1e5b7a
Stephanie Youstra <steph.you07@gmail.com>
2018-03-19 10:52:34 -0400

    feat: Update the onBindViewHolder to account for the entire cursor (Lesson 8.12)

--- Lesson07-Waitlist/T07.04-Exercise-UpdateTheAdapter/app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
+++ Lesson07-Waitlist/T07.04-Exercise-UpdateTheAdapter/app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
@@ -39,14 +39,22 @@ public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.Gues
     @Override
     public void onBindViewHolder(GuestViewHolder holder, int position) {
         // TODO (5) Move the cursor to the passed in position, return if moveToPosition returns false
+        if (!(mCursor.moveToPosition(position))) {
+            return;
+        } else {
 
-        // TODO (6) Call getString on the cursor to get the guest's name
+            // TODO (6) Call getString on the cursor to get the guest's name
+            String guestName = mCursor.getString(mCursor.getColumnIndex(WaitlistContract.WaitlistEntry.COLUMN_GUEST_NAME));
 
-        // TODO (7) Call getInt on the cursor to get the party size
+            // TODO (7) Call getInt on the cursor to get the party size
+            int partySize = mCursor.getInt(mCursor.getColumnIndex(WaitlistContract.WaitlistEntry.COLUMN_PARTY_SIZE));
 
-        // TODO (8) Set the holder's nameTextView text to the guest's name
+            // TODO (8) Set the holder's nameTextView text to the guest's name
+            holder.nameTextView.setText(guestName);
 
-        // TODO (9) Set the holder's partySizeTextView text to the party size
+            // TODO (9) Set the holder's partySizeTextView text to the party size
+            holder.partySizeTextView.setText(String.valueOf(partySize));
+        }
     }
 
     @Override
