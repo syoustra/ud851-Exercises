7f428c6138fee1f633c77361a66b1893c2e6f236
Stephanie Youstra <steph.you07@gmail.com>
2018-03-19 01:29:44 -0400

    fix: Fix public static final class (Lesson 8.05)

--- Lesson07-Waitlist/T07.01-Exercise-CreateAContract/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
+++ Lesson07-Waitlist/T07.01-Exercise-CreateAContract/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
@@ -7,7 +7,7 @@ public class WaitlistContract {
 
 
     // TODO (1) Create an inner class named WaitlistEntry class that implements the BaseColumns interface
-    public class WaitlistEntry implements BaseColumns {
+    public static final class WaitlistEntry implements BaseColumns {
 
         // TODO (2) Inside create a static final members for the table name and each of the db columns
         // TABLE_NAME -> waitlist;
