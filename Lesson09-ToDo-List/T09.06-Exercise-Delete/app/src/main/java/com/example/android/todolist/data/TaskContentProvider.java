00f0c8f30418978019f7b26e21b215724acbe2ff
Stephanie Youstra <steph.you07@gmail.com>
2018-03-22 19:32:30 -0400

    feat: Recognize a single item and delete that row (Lesson 10.28)

--- Lesson09-ToDo-List/T09.06-Exercise-Delete/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ Lesson09-ToDo-List/T09.06-Exercise-Delete/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -157,13 +157,36 @@ public class TaskContentProvider extends ContentProvider {
     public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
 
         // TODO (1) Get access to the database and write URI matching code to recognize a single item
+        final SQLiteDatabase db = mTaskDbHelper.getReadableDatabase();
+
 
         // TODO (2) Write the code to delete a single row of data
         // [Hint] Use selections to delete an item by its row ID
+        int match = sUriMatcher.match(uri);
+        Cursor retCursor;
+        int deletedTasks = 0;
+
+        switch (match) {
+            case TASK_WITH_ID:
+                String id = uri.getPathSegments().get(1);
+
+                String mSelection = "_id=?";
+                String[] mSelectionArgs = new String[]{id};
+
+                db.delete(TaskContract.TaskEntry.TABLE_NAME, mSelection, mSelectionArgs);
+                deletedTasks ++;
+
+                break;
+        }
 
         // TODO (3) Notify the resolver of a change and return the number of items deleted
+        if (deletedTasks != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+            return deletedTasks;
+        }
 
-        throw new UnsupportedOperationException("Not yet implemented");
+
+
     }
 
 
