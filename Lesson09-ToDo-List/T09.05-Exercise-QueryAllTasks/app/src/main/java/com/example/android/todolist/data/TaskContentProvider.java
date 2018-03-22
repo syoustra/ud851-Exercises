cdf8e4e09a5cb1988e555c5caf13bbd0ee2a3fe0
Stephanie Youstra <steph.you07@gmail.com>
2018-03-21 23:31:55 -0400

    feat: Add query to return cursor of all the tasks (Lesson 10.22)

--- Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -121,14 +121,25 @@ public class TaskContentProvider extends ContentProvider {
                         String[] selectionArgs, String sortOrder) {
 
         // TODO (1) Get access to underlying database (read-only for query)
+        final SQLiteDatabase db = mTaskDbHelper.getReadableDatabase();
 
         // TODO (2) Write URI match code and set a variable to return a Cursor
+        int match = sUriMatcher.match(uri);
 
         // TODO (3) Query for the tasks directory and write a default case
+        Cursor retCursor;
+        switch (match) {
+            case TASKS:
+                retCursor = db.query(TABLE_NAME, projection, selection, selectionArgs, null, null, sortOrder);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri " + uri);
+        }
 
         // TODO (4) Set a notification URI on the Cursor and return that Cursor
+        retCursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return retCursor;
 
-        throw new UnsupportedOperationException("Not yet implemented");
     }
 
 
